2f618ddde9c7b950eead6e50e217b22381319a0a
RaphaelBovi <93729728+RaphaelBovi@users.noreply.github.com>
2022-07-04 14:45:06 -0300

    Cliente

--- src/main/java/com/cadastro/cliente/Cliente/controller/ClienteController.java
+++ src/main/java/com/cadastro/cliente/Cliente/controller/ClienteController.java
@@ -4,13 +4,15 @@ import com.cadastro.cliente.Cliente.controller.dtos.ClienteRequestDTO;
 import com.cadastro.cliente.Cliente.controller.dtos.ClienteResponseDTO;
 import com.cadastro.cliente.Cliente.entity.Cliente;
 import com.cadastro.cliente.Cliente.service.ClienteService;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
+@Controller
 @RestController
 @RequestMapping(value = "/cliente")
 public class ClienteController {
